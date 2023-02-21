95b4efb19c1342cb851ce227c22f0b2df5087b45
Lucas Oliveira <lucasgoliveiras@gmail.com>
2023-02-21 10:41:15 -0300

    fixing the first question

--- listas_e_pilhas/CadernoInteligente/caderno_inteligente.py
+++ listas_e_pilhas/CadernoInteligente/caderno_inteligente.py
@@ -2,16 +2,23 @@
 def verificar_pilha(pilha):
     #Variável que mantém o controle das capas
     pilha_temporaria = []
+    
+    #Laço que percorre a pilha de capas e seus índices
     for i, capa in enumerate(pilha):
         if capa == "F":
             pilha_temporaria.append(i)
+            
+        elif not pilha_temporaria:
+          return f"Incorreto, devido a capa na posição {i+1}."
+          
         else:
-            if not pilha_temporaria:
-                return f"Incorreto, devido a capa na posição {i + 1}."
-            pilha_temporaria.pop()
+          pilha_temporaria.pop(0)
+          
+    #Se a lista não estiver vazia, o caderno está incorreto
     if pilha_temporaria:
-        return f"Incorreto, devido a capa na posição {pilha_temporaria[-1] + 1}."
-    return "Correto."
+        return f"Incorreto, devido a capa na posição {pilha_temporaria[0] + 1}."
+    else:
+      return "Correto."
 
 #Recebendo a pilha de capas e chamando a função
 pilha = input().strip()
